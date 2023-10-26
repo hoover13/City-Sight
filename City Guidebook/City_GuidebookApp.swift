98cef8740e8afa5ddaf27d4a10c3b699bf3a0304
Min Thu Khine <helloworldjustin9@gmail.com>
2023-10-26 01:21:54 -0400

    Initial Commit

---
+++ City Guidebook/City_GuidebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_GuidebookApp.swift
+//  City Guidebook
+//
+//  Created by Hoover on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct City_GuidebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
